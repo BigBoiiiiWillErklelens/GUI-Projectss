f241d614559865c6795b19e5aed14d55d489ff75
BigBoiiiiWillErklelens <77988690+BigBoiiiiWillErklelens@users.noreply.github.com>
2021-05-03 13:39:41 -0600

    Add files via upload

---
+++ tKinkterTest.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+top = Tk()
+
+L1 = label(top, text="Hello, World!")
+
+L1.grid(column= 0, row= 1)
+
+E1 = Entry(top, bd = 5)
+
+E1.grid(column+ 0, row = 2)
+
+print E1.get())
+
+
+
+
+
+top.mainloop()
